3f9bd5a34efabf4761adc2a4704cf68583323c8d
Loading <traftae@gmail.com>
2025-10-08 17:01:19 -0400

    Remove `spanall.js`: delete unused JS file for auto-wrapping text.

--- js/spanall.js
+++
@@ -1,7 +0,0 @@
-// Auto-wrap each letter
-document.querySelectorAll(".MyName").forEach(el => {
-    el.innerHTML = el.textContent
-        .split("")
-        .map(c => `<span>${c}</span>`)
-        .join("");
-});
